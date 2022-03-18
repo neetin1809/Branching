9babacbc4185662bc2b7f4288abb8daab1456148
Nitin Vaswani <nitin.vaswani@rediffmail.com>
2022-03-19 00:24:54 +0530

    #Addec Nested branching

--- Program.cs
+++ Program.cs
@@ -27,8 +27,40 @@ void handleSecondBranch(IApplicationBuilder app)
     });
 }
 
+// Delegate implementation for app.Map("/first", handleFirstBranch)
 void handlefirstBranch(IApplicationBuilder app)
-{
+{   //first/level-2
+    app.Map("/level-2", app =>
+    {
+        //further level-3 i.e first/level-2/level-3
+        app.Map("/level-3", app =>
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Branch: first/level-1/level-2/level-3");
+            });
+        });
+
+        app.Run(async context =>
+        {
+            await context.Response.WriteAsync("Branch: first/level-1/level-2");
+        });
+    });
+    app.Map("/step-2/step-3", app =>
+     {
+         app.Map("/step-4", app =>
+          {
+              app.Run(async context =>
+              {
+                  await context.Response.WriteAsync("Branch: /step-2/step-3/step-4");
+              });
+          });
+         app.Run(async context =>
+         {
+             await context.Response.WriteAsync("Branch: first/step-2/step-3");
+         });
+     });
+    //localhost/first
     app.Run(async context =>
     {
         await context.Response.WriteAsync("First Branch is executed");
